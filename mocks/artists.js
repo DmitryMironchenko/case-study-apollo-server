71cf1eee9c2ca28e9021c0726698a66da72ecc1a
Dmitry Mironchenko <d.mironchenko@gmail.com>
2021-07-15 12:31:13 +0200

    Add filtering by query

--- mocks/artists.js
+++ mocks/artists.js
@@ -1,5 +1,7 @@
 const { data: { artists } } = require('./artists.json')
 
 module.exports = {
-  artists: (parent, { limit = 10, offset = 0 }) => artists.slice(offset, limit + offset)
+  artists: (parent, { query, limit = 10, offset = 0 }) => artists
+    .filter(a => !query || new RegExp(query, 'gi').test(a.name) )
+    .slice(offset, limit + offset)
 }
